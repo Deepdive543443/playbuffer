0373757842a8fa506acb41f10e90601570dd11cb
Qihang Feng <83911295+Deepdive543443@users.noreply.github.com>
2023-09-07 22:40:17 +0100

    Load image and ID

--- HelloWorld/MainGame.cpp
+++ HelloWorld/MainGame.cpp
@@ -6,6 +6,16 @@ int DISPLAY_WIDTH = 640;
 int DISPLAY_HEIGHT = 360;
 int DISPLAY_SCALE = 2;
 
+
+struct GameState
+{
+	float timer = 0.0f;
+	int spriteID = 0;
+};
+
+GameState game_state;
+
+
 // The entry point for a PlayBuffer program
 void MainGameEntry( PLAY_IGNORE_COMMAND_LINE )
 {
@@ -16,7 +26,24 @@ void MainGameEntry( PLAY_IGNORE_COMMAND_LINE )
 bool MainGameUpdate( float elapsedTime )
 {
 	Play::ClearDrawingBuffer( Play::cOrange );
-	Play::DrawDebugText( { DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2 }, "Good looking font style!" );
+
+	// Obtain information from struct
+	Play::DrawDebugText(
+		{ DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2 }, 
+		Play::GetSpriteName(game_state.spriteID), 
+		Play::cWhite
+	);
+	
+	// Get mouse position
+	Play::DrawSprite(game_state.spriteID, Play::GetMousePos(), game_state.timer);
+
+	// Add one to Id when pressing space
+	if ( Play::KeyPressed(VK_SPACE) )
+	{
+		game_state.spriteID++;
+	}
+
+
 	Play::PresentDrawingBuffer();
 	return Play::KeyDown( VK_ESCAPE );
 }
