5489aac2d4e169485fb02648337cfc944ee46fba
Justin Fung <qf28@sussex.ac.uk>
2023-09-09 21:12:44 +0100

    Add fan

--- HelloWorld/MainGame.cpp
+++ HelloWorld/MainGame.cpp
@@ -4,7 +4,7 @@
 
 int DISPLAY_WIDTH = 1280;
 int DISPLAY_HEIGHT = 720;
-int DISPLAY_SCALE = 2;
+int DISPLAY_SCALE = 1;
 
 
 struct GameState
@@ -18,38 +18,13 @@ enum GameObjectType
 {
 	TYPE_NULL = -1,
 	TYPE_AGENT8,
+	TYPE_FAN,
+	TYPE_TOOL,
+	TYPE_COIN,
+	TYPE_STAR,
+	TYPE_LASER,
+	TYPE_DESTROYED,
 };
-
-void HandlePlayercontrols();
-
-
-// The entry point for a PlayBuffer program
-void MainGameEntry( PLAY_IGNORE_COMMAND_LINE )
-{
-	Play::CreateManager( DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_SCALE );
-	Play::CentreAllSpriteOrigins();
-	Play::LoadBackground("Data\\Backgrounds\\background.png");
-	Play::StartAudioLoop("music");
-	Play::CreateGameObject(TYPE_AGENT8, {115, 0}, 50, "agent8");
-}
-
-// Called by PlayBuffer every frame (60 times a second!)
-bool MainGameUpdate( float elapsedTime )
-{
-	Play::DrawBackground();
-	HandlePlayercontrols();
-
-	// Quit application
-	Play::PresentDrawingBuffer();
-	return Play::KeyDown( VK_ESCAPE );
-}
-
-// Gets called once when the player quits the game 
-int MainGameExit( void )
-{
-	Play::DestroyManager();
-	return PLAY_OK;
-}
 
 void HandlePlayercontrols()
 {
@@ -64,7 +39,7 @@ void HandlePlayercontrols()
 		obj_agent8.acceleration = { 0, 1 };
 		Play::SetSprite(obj_agent8, "agent8_fall", 0);
 	}
-	else 
+	else
 	{
 		Play::SetSprite(obj_agent8, "agent8_hang", 0.02f);
 		obj_agent8.velocity *= 0.5f;
@@ -76,6 +51,55 @@ void HandlePlayercontrols()
 	Play::UpdateGameObject(obj_agent8);
 
 	if (Play::IsLeavingDisplayArea(obj_agent8)) obj_agent8.pos = obj_agent8.oldPos;
-	Play::DrawLine({obj_agent8.pos.x, 0}, obj_agent8.pos, Play::cWhite);
+	Play::DrawLine({ obj_agent8.pos.x, 0 }, obj_agent8.pos, Play::cWhite);
 	Play::DrawObjectRotated(obj_agent8);
+};
+void UpdateFan()
+{
+	GameObject& obj_fan = Play::GetGameObjectByType(TYPE_FAN);
+	Play::DrawObject(obj_fan);
+};
+void UpdateTools()
+{
+	GameObject& obj_agent8 = Play::GetGameObjectByType(TYPE_AGENT8);
+	std::vector<int> vTools = Play::CollectGameObjectIDsByType(TYPE_TOOL);
+
+	for (int id: vTools)
+	{
+		GameObject& obj_tool = Play::GetGameObject(id);
+	}
+};
+
+// This looks like unity, I like it
+// The entry point for a PlayBuffer program
+void MainGameEntry( PLAY_IGNORE_COMMAND_LINE )
+{
+	Play::CreateManager( DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_SCALE );
+	Play::CentreAllSpriteOrigins();
+	Play::LoadBackground("Data\\Backgrounds\\background.png");
+	Play::StartAudioLoop("music");
+	Play::CreateGameObject(TYPE_AGENT8, {115, 0}, 50, "agent8");
+	int id_fan = Play::CreateGameObject(TYPE_FAN, {1140, 217}, 0, "fan");
+	Play::GetGameObject(id_fan).velocity = {0, 3};
+	Play::GetGameObject(id_fan).animSpeed = 1.0f;
+}
+
+// Called by PlayBuffer every frame (60 times a second!)
+bool MainGameUpdate( float elapsedTime )
+{
+	Play::DrawBackground();
+	HandlePlayercontrols();
+	UpdateFan();
+	UpdateTools();
+
+	// Quit application
+	Play::PresentDrawingBuffer();
+	return Play::KeyDown( VK_ESCAPE );
+}
+
+// Gets called once when the player quits the game 
+int MainGameExit( void )
+{
+	Play::DestroyManager();
+	return PLAY_OK;
 }
